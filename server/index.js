07da92363d57384e740e8fce403c359e17044a77
apexmotorsdev <apex.motors.dev@gmail.com>
2026-07-03 20:27:01 +0200

    log check test

--- server/index.js
+++ server/index.js
@@ -79,3 +79,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server listening on port ${PORT}`);
 });
+console.log("Полученные данные:", { date, time });
